77f4b17db230480c66751ac761452ab7e126e06c
saliery22 <129659617+saliery22@users.noreply.github.com>
2024-05-27 11:07:34 +0300

    Update zup.js

--- zup.js
+++ zup.js
@@ -2595,7 +2595,6 @@ let rows = document.querySelectorAll('#monitoring_table tr');
  str.forEach((element) => {if(nametr.indexOf(element)>=0){
  pereizd=0;
  robota=0;
- let pr=0,rob =0;
  stroka=[];
    for (let ii = Global_DATA[i].length-1; ii>0; ii-=30){
    points = 0;
@@ -2617,8 +2616,25 @@ let rows = document.querySelectorAll('#monitoring_table tr');
     if(points>100 ){robota++;pereizd=0;
      //let tooltip = L.tooltip([y,x], {content: 'роб',permanent: true}).addTo(map);
     }
-     if(pereizd==2){if(pr==0){stroka.push('пер');}pr=1;rob=0;}
-     if(robota==2){if(rob==0){stroka.push('роб');}rob=1;pr=0;}
+     if(pereizd==2){
+     if(stroka.length>0){
+     if(stroka[stroka.length-1]=='роб'){
+     stroka.push('пер');
+     }
+     }else{
+      stroka.push('пер');
+     }
+     }
+     if(robota==2){
+     if(stroka.length>0){
+     if(stroka[stroka.length-1]=='пер'){
+     stroka.push('роб');
+     }
+     }else{
+      stroka.push('роб');
+     }
+     }
+    
  }
  
   if(stroka.length>0){
@@ -2628,25 +2644,22 @@ let rows = document.querySelectorAll('#monitoring_table tr');
  if(rows.length>0){
   for(let v = 0; v<rows.length; v++){
   if(rows[v].cells[0].textContent==nametr){
-   ind=stroka.length-(rows[v].length-1);
+   let ind=stroka.length-(rows[v].length-1);
    for(let vv = ind-1; vv>=0; vv--){
-   if(rows[v].cells[1].textContent!=stroka[stroka.length -vv-1]){
     rows[v].insertRow(1);
-    rows[v].cells[1].textContent=stroka[stroka.length -vv-1];
+    rows[v].cells[1].textContent=stroka[vv];
     rows[v].cells[1].style.backgroundColor = "#f8b1c0";
-   }
-  
    }
    break;
   }else{
     if(v==rows.length-1){
-   for(let v = 0; v<stroka.length; v++){strr= "<td bgcolor = '#f8b1c0'>"+stroka[v]+"</td>"+strr;}
+   for(let v = 0; v<stroka.length; v++){strr+= "<td bgcolor = '#f8b1c0'>"+stroka[v]+"</td>";}
     $("#monitoring_table").append("<tr id="+id+"><td>"+nametr+"</td>"+strr+"</tr>");
        }
    }
   }
   }else{
-  for(let v = 0; v<stroka.length; v++){strr= "<td bgcolor = '#f8b1c0'>"+stroka[v]+"</td>"+strr;}
+  for(let v = 0; v<stroka.length; v++){strr+= "<td bgcolor = '#f8b1c0'>"+stroka[v]+"</td>";}
     $("#monitoring_table").append("<tr id="+id+"><td>"+nametr+"</td>"+strr+"</tr>");
   }
  }
